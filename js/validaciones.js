cf2c13a29e0d7a4f3b8653ea29b398b1abba3452
jonhatan <A01769156@tec.mx>
2023-08-14 20:20:55 -0600

    clase 03 completa

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,29 +1,30 @@
-export function valida(input){
+export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
-    if(validadores[tipoDeInput]){
-        validadores[tipoDeInput](input);
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
     }
-}
-
-const validadores = {
-    nacimiento: input => validarNacimiento(input),
-}
-
-function validarNacimiento(input){
-    const fechaCliente = input.value;
+  }
+  
+  const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+  };
+  
+  function validarNacimiento(input) {
+    const fechaCliente = new Date(input.value);
     let mensaje = "";
-    if(!mayorDeEdad(fechaCliente)){
-        mensaje = "Debes ser mayor de 18 años";
+    if (!mayorDeEdad(fechaCliente)) {
+      mensaje = "Debes tener al menos 18 años de edad";
     }
+  
     input.setCustomValidity(mensaje);
-};
-
-function mayorDeEdad(fecha){
+  }
+  
+  function mayorDeEdad(fecha) {
     const fechaActual = new Date();
     const diferenciaFechas = new Date(
-        fecha.getUTCFullYear() + 18, 
-        fecha.getUTCMounth(), 
-        fecha.getUTCDate()
+      fecha.getUTCFullYear() + 18,
+      fecha.getUTCMonth(),
+      fecha.getUTCDate()
     );
-    return (diferenciaFechas <= fechaActual);
-}
+    return diferenciaFechas <= fechaActual;
+  }
